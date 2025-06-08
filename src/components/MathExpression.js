9a1022d7003e8fd680fdf86614a27f6ab82b5af1
Comodo5000 <inteligenciaartificial368@gmail.com>
2025-06-07 22:56:05 -0600

    Update MathExpression.js

--- src/components/MathExpression.js
+++ src/components/MathExpression.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import MathJax from 'react-mathjax-preview';
+import { MathJax, MathJaxContext } from 'better-react-mathjax';
 
 export default function MathExpression({ expression }) {
   return (
-    <div className="p-2 bg-white rounded shadow text-center my-2">
-      <MathJax math={`f(x) = ${expression}`} />
-    </div>
+    <MathJaxContext config={{ loader: { load: ["input/asciimath", "output/chtml"] } }}>
+      <div className="p-2 bg-white rounded shadow text-center my-2">
+        <MathJax>{`\\( ${expression} \\)`}</MathJax>
+      </div>
+    </MathJaxContext>
   );
 }
